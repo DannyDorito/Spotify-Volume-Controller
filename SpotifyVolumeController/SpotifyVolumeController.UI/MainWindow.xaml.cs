f764e739ee81214e32bdedd0c679b7d4025ed3f2
John Allison <john@jallison.co.uk>
2022-12-02 21:42:38 +0000

    basic ui and mousehook

---
+++ SpotifyVolumeController/SpotifyVolumeController.UI/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Gma.System.MouseKeyHook;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SpotifyVolumeController.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IKeyboardMouseEvents GlobalHook;
+
+        private readonly int DeltaOffset = -120;
+
+        private bool IsDebug { get => bool.Parse(ConfigurationManager.AppSettings["Debug"]); }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DebugBox.IsEnabled = IsDebug;
+
+            var golbalHookSuccessful = SubscribeGlobalHook();
+            if (IsDebug)
+            {
+                DebugBox.Items.Add($"Hook bound: {golbalHookSuccessful}");
+            }
+        }
+
+        #region Keyboard Hook
+
+
+        public bool SubscribeGlobalHook()
+        {
+            try
+            {
+                GlobalHook = Hook.GlobalEvents();
+                GlobalHook.MouseHWheel += GlobalMouseHWheel;
+                return true;
+            }
+            catch
+            {
+                Unsubscribe();
+                return false;
+            }
+        }
+
+        private void GlobalMouseHWheel(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (IsDebug)
+            {
+                DebugBox.Items.Add($"{mouseEventArgs.Delta / DeltaOffset} {mouseEventArgs.X},{mouseEventArgs.Y} {mouseEventArgs.Location}");
+            }
+        }
+
+        public void Unsubscribe()
+        {
+            if (GlobalHook != null)
+            {
+                GlobalHook.MouseHWheel -= GlobalMouseHWheel;
+
+                GlobalHook.Dispose();
+                if (IsDebug)
+                {
+                    DebugBox.Items.Add($"Hook unbound: true");
+                }
+            }
+        }
+
+        #endregion Keyboard Hook
+
+        #region UI Event Handlers
+
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Unsubscribe();
+        }
+
+        private void Auth_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion UI Event Handlers
+    }
+}
